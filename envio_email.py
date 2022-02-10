7a8aa201afe46bce1ab7dec7d165185a01983e01
asmutti <amutti.dev@gmail.com>
2022-02-09 23:46:58 -0300

    commit primeiro draft

---
+++ envio_email.py
@@ -0,0 +1,25 @@
+import smtplib 
+
+try:
+        
+    msgFrom = str(input())
+    print("tentando conexao")
+    smtpObj = smtplib.SMTP('smtp.gmail.com')
+    print("conectado")
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    emailLogin = 'amutti.dev@gmail.com'
+    senhaLogin = ''
+    print("tentando login")
+    smtpObj.login(emailLogin, senhaLogin)
+    print("usuario logado")
+
+    msg = ""
+    smtpObj.sendmail("amutti.dev@gmail.com", msgFrom, 'Subject: subject\n{}'.format(msg))
+    smtpObj.quit()
+    print("email enviado com sucesso")
+
+
+except:
+    print("erro")
+

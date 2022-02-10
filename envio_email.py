5a6d64d2a23acb017b81690474f3503ce2d18d52
asmutti <amutti.dev@gmail.com>
2022-02-10 00:14:39 -0300

    commit primeiro draft

--- envio_email.py
+++ envio_email.py
@@ -1,20 +1,20 @@
 import smtplib 
 
 try:
-    msgFrom = str(input())
+    msgFrom = str(input("Insira o email de destino: "))
     print("tentando conexao")
-    smtpObj = smtplib.SMTP('smtp.gmail.com')
+    smtpObj = smtplib.SMTP('smtp-mail.outlook.com')
     print("conectado")
     smtpObj.ehlo()
     smtpObj.starttls()
-    emailLogin = 'amutti.dev@gmail.com'
-    senhaLogin = ''
+    emailLogin = 'mutti_angelo23@hotmail.com'
+    senhaLogin = 'senha'
     print("tentando login")
     smtpObj.login(emailLogin, senhaLogin)
     print("usuario logado")
 
-    msg = ""
-    smtpObj.sendmail("amutti.dev@gmail.com", msgFrom, 'Subject: subject\n{}'.format(msg))
+    msg = """"""
+    smtpObj.sendmail("mutti_angelo23@hotmail.com", msgFrom, 'Subject: subject\n{}'.format(msg))
     smtpObj.quit()
     print("email enviado com sucesso")
 

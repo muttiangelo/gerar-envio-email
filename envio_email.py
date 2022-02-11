72222dbd66e0219cc3ffc38978286ddbac32aaaa
asmutti <amutti.dev@gmail.com>
2022-02-10 23:55:39 -0300

    commit primeiro draft

--- envio_email.py
+++ envio_email.py
@@ -1,24 +1,41 @@
+import email
 import smtplib 
+import json
 
-try:
-    msgFrom = str(input("Insira o email de destino: "))
-    print("tentando conexao")
-    smtpObj = smtplib.SMTP('smtp-mail.outlook.com')
-    print("conectado")
-    smtpObj.ehlo()
-    smtpObj.starttls()
-    emailLogin = 'mutti_angelo23@hotmail.com'
-    senhaLogin = 'senha'
-    print("tentando login")
-    smtpObj.login(emailLogin, senhaLogin)
-    print("usuario logado")
+class envio_email():
 
-    msg = """"""
-    smtpObj.sendmail("mutti_angelo23@hotmail.com", msgFrom, 'Subject: subject\n{}'.format(msg))
-    smtpObj.quit()
-    print("email enviado com sucesso")
+    def __init__(self):
+        f = open('parametros.json', 'r')
+        dados = json.load(f)
+        self.emailDestino =  dados["email_destino"]
+        self.email = dados["email"]
+        self.senha = dados["senha"]
 
 
-except:
-    print("erro")
+    def execute(self, listaDados):
+        try:
+            open("parametros.json")
+            msgFrom = self.emailDestino
+            print("tentando conexao")
+            smtpObj = smtplib.SMTP('smtp-mail.outlook.com')
+            print("conectado")
+            smtpObj.ehlo()
+            smtpObj.starttls()
+            emailLogin = self.email
+            senhaLogin = self.senha
+            print("tentando login")
+            smtpObj.login(emailLogin, senhaLogin)
+            print("usuario logado")
 
+            msg = """"""
+            smtpObj.sendmail("mutti_angelo23@hotmail.com", msgFrom, 'Subject: test\n{}'.format(msg))
+            smtpObj.quit()
+            print("email enviado com sucesso")
+
+
+        except:
+            print("erro")
+
+if __name__ == "__main__":
+    a = envio_email()
+    a.execute()
